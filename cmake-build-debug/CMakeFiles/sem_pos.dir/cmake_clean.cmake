cd6b8b240ac1e1ecd88fbf594dc61a9333366b62
Jakub969 <jakub.hrubizna@gmail.com>
2023-12-28 13:28:07 +0100

    prvy commit

---
+++ cmake-build-debug/CMakeFiles/sem_pos.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sem_pos.dir/main.c.o"
+  "sem_pos"
+  "sem_pos.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sem_pos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
